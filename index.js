6aa3745aac22a478d084d484bee8306cabace2bc
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-06 23:39:33 +0300

    commit tree ec0c3332b33e1ca2abba830ef457ac3c44a9b8ed

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(feetValue){
   // Write your code here!
+  let message;
+  if (feetValue<=400){
+    message = "This one is on me!";    
+  }
+  else if (feetValue>2000 && feetValue<=2500){
+    message = "I will gladly take your thirty bucks.";
+  }
+  else if (feetValue>2500){
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(NYC){
   // Write your code here!
+  let city;
+  let message;
+  NYC === city? (message = "Ok, sounds good.") : NYC!==city? (message = "No go."):(message="Ok, sounds good.");
+  //let messageTwo = city === NYC? "Ok, Sounds good." : "No go. ";
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch(tip){
+    case 'generous':
+      message = "Thank you so much."
+      break;
+    case 'not as generous':
+      message = "Thank you.";
+      break;
+      default:
+        message = "Bye.";
+        break;
+  }
+  return message;
 }
